52c5fde04daaa35f07402152c23fed2876cf1ab0
Amandine3 <etu16751@yahoo.fr>
2015-01-04 17:44:29 +0100

    add_contact sql : modification

--- doc/sql/procedures/add_contact.sql
+++ doc/sql/procedures/add_contact.sql
@@ -1,5 +1,4 @@
-create or replace function add_contact(int,text,text,text,text) 
-returns void
+create or replace function add_contact (int,text,text,text,text) returns integer
 as
 '
   declare f_sexe alias for $1;
@@ -7,11 +6,19 @@ as
   declare f_prenom alias for $3;
   declare f_comm alias for $4;
   declare f_email alias for $5;
-
+  declare retour integer;
+  declare id integer;
 begin
  	insert into contact(sexe,nom,prenom,comm,email) 
 	values (f_sexe,f_nom,f_prenom,f_comm,f_email);
-return;
+        select into id idcontact from contact where sexe=f_sexe and nom=f_nom 
+        and prenom=f_prenom and comm=f_comm and email=f_email;
+        if not found	then
+		retour=0;
+	else 
+		retour=1;
+	end if;
+        return retour;
 end;
 '
 language 'plpgsql';
